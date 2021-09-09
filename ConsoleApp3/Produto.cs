ba277627709fb8904cacb8769a539c619f27c2ed
Kiberio <dhiroshi3@gmail.com>
2021-09-08 22:53:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/Produto.cs
@@ -0,0 +1,54 @@
+﻿//Daniel Hiroshi Fugikawa - 200209
+using System.Globalization;
+
+
+namespace ConsoleApp3
+{
+    public class produto
+    {
+        //Declaração das variaveis do produto
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Declaração do construtor
+        public produto (string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Função para calcular o valor total do estoque
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //Função para adicionar Produtos ao estoque
+        public void AdicionarProduto(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        //Função para remover produtos do estoque
+        public void RemoverProduto(int qtd)
+        {
+            Quantidade = Quantidade - qtd;
+        }
+
+        //Criando um formato para o ToString
+        public override string ToString()
+        {
+            return Nome
+            + ",R$"
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ","
+            + Quantidade
+            +" unidades,Total: R$"
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+    
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿//Daniel Hiroshi Fugikawa - 200209
+using System;
+using System.Globalization;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicitar o as informações do produto 
+            Console.WriteLine("Entre com os dados do produto");
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            //Declaração de variavel de classe
+            produto p = new produto(nome, preco, quantidade);
+           
+            //Imprimir os dados do estoque
+            Console.WriteLine("Dados do produto: " + p);
+
+            //Solicitando o número de produto ao qual o usuario deseja adicionar ao estoque
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+
+            //Imprimir os dados atualizados do estoque
+            Console.WriteLine("Dados atualizados: " + p);
+
+            //Solicitar o número de produtos ao qual o usuario deseja retirar do estoque
+            Console.WriteLine("Digite o número de produtos a ser retirado do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+
+            //Imprimmir os dados atualizados do estoque 
+            Console.WriteLine("Dados atualizados: " + p);
+
+        }
+    }
+}

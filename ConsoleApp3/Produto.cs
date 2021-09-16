934481cb037d6738a98e87122076960892853e10
Kiberio <dhiroshi3@gmail.com>
2021-09-15 21:15:47 -0300

    Novo branch

--- ConsoleApp3/Produto.cs
+++ ConsoleApp3/Produto.cs
@@ -7,52 +7,75 @@ namespace ConsoleApp3
     public class produto
     {
         //Declaração das variaveis do produto
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         //Declaração do construtor
-        public produto (string nome, double preco, int quantidade)
+        public produto(string nome, double preco, int quantidade)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
         }
 
-        //contrutor padrão
-        public produto() { 
+        //contrutor padrao
+        public produto()
+        {
+
+        }
+
+        //utilização de GET e Set
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
         }
 
         //Função para calcular o valor total do estoque
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         //Função para adicionar Produtos ao estoque
         public void AdicionarProduto(int qtd)
         {
-            Quantidade += qtd;
+            _quantidade += qtd;
         }
 
         //Função para remover produtos do estoque
         public void RemoverProduto(int qtd)
         {
-            Quantidade = Quantidade - qtd;
+            _quantidade = _quantidade - qtd;
         }
 
         //Criando um formato para o ToString
         public override string ToString()
         {
-            return Nome
+            return _nome
             + ",R$"
-            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
             + ","
-            + Quantidade
-            +" unidades,Total: R$"
+            + _quantidade
+            + " unidades,Total: R$"
             + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
 
         }
     }
-    
+
 }
